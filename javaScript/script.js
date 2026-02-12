e933e743acf92b4c029b0257deb054faa9b3307a
Euan-Benson <euanbson@gmail.com>
2026-02-12 12:56:45 +0000

    remove hidden id

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,16 +1,6 @@
-const hidden = document.getElementById("hidden");
 const darkMode = document.getElementById("darkBtn");
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
-
-//hides section but cant unhide it
-function hideSection() {
-  if (hidden.style.display === "none") {
-    hidden.style.display = "block";
-  } else {
-    hidden.style.display = "none";
-  }
-}
 
 function hamburgerMenu() {
   hamMenu.classList.toggle("active");
@@ -30,5 +20,4 @@ function toggleDark() {
 }
 
 hamMenu.addEventListener("click", hamburgerMenu);
-//hidden.addEventListener("click", hideSection);
 darkMode.addEventListener("click", toggleDark);
