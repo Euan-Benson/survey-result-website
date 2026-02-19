9d37bf96d63d1ea480702533be18a3a1e5f388ae
Euan-Benson <euanbson@gmail.com>
2026-02-19 14:38:49 +0000

    add modal image to visuals page

--- javaScript/script.js
+++ javaScript/script.js
@@ -10,7 +10,15 @@ const ageImg = document.getElementById("ageImg");
 const hoursImg = document.getElementById("hoursImg");
 const settingsImg = document.getElementById("settingsImg");
 
+const visModal = document.getElementById("visModal");
+const visModalImg = document.getElementById("img02");
+const visCaption = document.getElementById("visCaption");
+const currVis = document.getElementById("currVis");
+const impVis = document.getElementById("impVis");
+
 const span = document.getElementsByClassName("close")[0];
+const spanVis = document.getElementsByClassName("close")[0];
+
 
 function hamburgerMenu() {
   hamMenu.classList.toggle("active");
@@ -35,14 +43,28 @@ function modalImage(){
   captionText.innerHTML = this.alt;
 }
 
+function visModalImage(){
+  visModal.style.display = "block";
+  visModalImg.src = this.src;
+  visCaption.innerHTML = this.alt;
+}
+
 function closeImage(){
   modal.style.display = "none";
 }
 
+function closeVisImage(){
+  visModal.style.display = "none";
+}
+
 hamMenu.addEventListener("click", hamburgerMenu);
 darkMode.addEventListener("click", toggleDark);
-ageImg.addEventListener("click", modalImage);
-hoursImg.addEventListener("click", modalImage);
-settingsImg.addEventListener("click", modalImage);
+
+// ageImg.addEventListener("click", modalImage);
+// hoursImg.addEventListener("click", modalImage);
+// settingsImg.addEventListener("click", modalImage);
+currVis.addEventListener("click", visModalImage);
+impVis.addEventListener("click", visModalImage);
 
 span.addEventListener("click", closeImage);
+spanVis.addEventListener("click", closeVisImage);
