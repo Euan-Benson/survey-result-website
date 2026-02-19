c73c56dc9c470e12ca79d01e0ad49e5807ad49ae
Euan-Benson <euanbson@gmail.com>
2026-02-19 15:17:02 +0000

    fix multi page modal images

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,22 +3,14 @@ const darkMode = document.getElementById("darkBtn");
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
-const modal = document.getElementById("ageModal");
-const modalImg = document.getElementById("img01");
+const modal = document.getElementById("imgModal");
+const modalImg = document.getElementById("imgExpand");
 const captionText = document.getElementById("caption");
-const ageImg = document.getElementById("ageImg");
-const hoursImg = document.getElementById("hoursImg");
-const settingsImg = document.getElementById("settingsImg");
-
-const visModal = document.getElementById("visModal");
-const visModalImg = document.getElementById("img02");
-const visCaption = document.getElementById("visCaption");
-const currVis = document.getElementById("currVis");
-const impVis = document.getElementById("impVis");
+const img01 = document.getElementById("img01");
+const img02 = document.getElementById("img02");
+const img03 = document.getElementById("img03");
 
 const span = document.getElementsByClassName("close")[0];
-const spanVis = document.getElementsByClassName("close")[0];
-
 
 function hamburgerMenu() {
   hamMenu.classList.toggle("active");
@@ -43,28 +35,15 @@ function modalImage(){
   captionText.innerHTML = this.alt;
 }
 
-function visModalImage(){
-  visModal.style.display = "block";
-  visModalImg.src = this.src;
-  visCaption.innerHTML = this.alt;
-}
-
 function closeImage(){
   modal.style.display = "none";
-}
-
-function closeVisImage(){
-  visModal.style.display = "none";
 }
 
 hamMenu.addEventListener("click", hamburgerMenu);
 darkMode.addEventListener("click", toggleDark);
 
-// ageImg.addEventListener("click", modalImage);
-// hoursImg.addEventListener("click", modalImage);
-// settingsImg.addEventListener("click", modalImage);
-currVis.addEventListener("click", visModalImage);
-impVis.addEventListener("click", visModalImage);
-
 span.addEventListener("click", closeImage);
-spanVis.addEventListener("click", closeVisImage);
+
+img01.addEventListener("click", modalImage);
+img02.addEventListener("click", modalImage);
+img03.addEventListener("click", modalImage);
