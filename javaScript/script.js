00e10a9ed65ab4c4fc9c1a6092299f3cd19dc9ab
Euan-Benson <euanbson@gmail.com>
2026-02-27 14:04:01 +0000

    fix bugs

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,12 +1,15 @@
-localStorage.getItem("theme");
-localStorage.setItem("theme", newTheme);
-const localStorageTheme = localStorage.getItem("theme");
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-const button = document.querySelector("[data-theme-toggle]");
-
+//hamburger menu
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+function hamburgerMenu() {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+}
+
+hamMenu.addEventListener("click", hamburgerMenu);
+
+//modal image
 const modal = document.getElementById("imgModal");
 const modalImg = document.getElementById("imgExpand");
 const captionText = document.getElementById("caption");
@@ -17,43 +20,6 @@ const img04 = document.getElementById("img04");
 
 const span = document.getElementsByClassName("close")[0];
 
-function hamburgerMenu() {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-}
-
-function calcSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
-  }
-
-  if (systemSettingDark.matches) {
-    return "dark";
-  }
-
-  return "light";
-}
-
-function toggleDark() {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-  const newCta =
-    newTheme === "dark" ? "Change the light theme" : "Change to dark theme";
-  button.innerText = newCta;
-
-  //button.setAttribute("aria-label", newCta);
-
-  document.querySelector("html").setAttribute("data-theme", newTheme);
-
-  localStorage.setItem("theme", newTheme);
-
-  currentThemeSetting = newTheme;
-}
-
-let currentThemeSetting = calcSettingAsThemeString({
-  localStorageTheme,
-  systemSettingDark,
-});
-
 function modalImage() {
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -63,9 +29,6 @@ function modalImage() {
 function closeImage() {
   modal.style.display = "none";
 }
-
-hamMenu.addEventListener("click", hamburgerMenu);
-button.addEventListener("click", toggleDark);
 
 span.addEventListener("click", closeImage);
 
