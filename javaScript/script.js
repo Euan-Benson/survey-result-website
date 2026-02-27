fadcc04c1dc210fcd8c168b488603079b4bd9302
Euan-Benson <euanbson@gmail.com>
2026-02-27 14:12:33 +0000

    add dark mode fn

--- javaScript/script.js
+++ javaScript/script.js
@@ -9,6 +9,60 @@ function hamburgerMenu() {
 
 hamMenu.addEventListener("click", hamburgerMenu);
 
+//dark mode
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+//finds current theme setting. Local storage --> system settings --> light mode
+function calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+}) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+//update button text and aria label (for icons)
+// a ? x : y -- this is an if else statement (one line notation)
+function updateButton({ buttonEl, isDark }) {
+  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+  buttonEl.setAttribute("aria-label", newCta);
+  buttonEl.innerText = newCta;
+}
+
+//update theme on html tag
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+function updateMode() {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+}
+
+let currentThemeSetting = calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+});
+
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+button.addEventListener("click", updateMode);
+
 //modal image
 const modal = document.getElementById("imgModal");
 const modalImg = document.getElementById("imgExpand");
